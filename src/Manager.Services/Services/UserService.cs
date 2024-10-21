9474b4c1feb11bcc162ffaf1d5d52453c0126d40
Samuel Esdras <esdrasamuel57@gmail.com>
2024-10-21 16:44:13 -0300

    Erros de references feitos, falta terminar os metodos do UserService

--- src/Manager.Services/Services/UserService.cs
+++ src/Manager.Services/Services/UserService.cs
@@ -1,27 +1,66 @@
+using AutoMapper;
+using Manager.Services.DTO;
 using Manager.Services.Interfaces;
+using Manager.Domain.Entities;
+using Manager.Core.Exceptions;
+using Manager.Infra.Interfaces;
+
 
 namespace Manager.Services.Services
 {
     public class UserService : IUserService
     {
         
+        public UserService(IMapper mapper, IUserRepository userRepository)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
+
         private readonly IMapper _mapper;
+
         
         private readonly IUserRepository _userRepository;
         
         public async Task<UserDTO> Create(UserDTO userDTO)
         {
+            var userExists = await _userRepository.GetByEmail(userDTO.Email);
+
+            if (userExists != null)
+            {
+                throw new DomainException("Email already exists");
+            }
             
+            var user = _mapper.Map<User>(userDTO);
+            user.Validate();
+            
+            var userCreated = await _userRepository.Create(user);
+            
+            return _mapper.Map<UserDTO>(userCreated);
         }
 
         public async Task<UserDTO> Update(UserDTO userDTO)
         {
+            var userExists = await _userRepository.Get(userDTO.Id);
+
+            if (userExists == null)
+            {
+                throw new DomainException("User not found");
+                
+            }
+            
+            var user = _mapper.Map<User>(userDTO);
+            user.Validate();
+                
+            var userUpdated = await _userRepository.Update(user);
+                
+            return _mapper.Map<UserDTO>(userUpdated);
             
         }
 
         public async Task Remove(long id)
         {
-            
+            await _userRepository.Remove(id);
         }
 
         public async Task<UserDTO> Get(long id)
